f89192d39853d958ee09d89535f37dc09bbf68c8
Fernando GJ <fernandogajib@gmail.com>
2024-09-19 23:37:55 +0200

    Look and Feel added

--- src/Init.java
+++ src/Init.java
@@ -3,7 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
+import com.formdev.flatlaf.FlatLightLaf;
 import controller.Controller;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import view.MainWindow;
 
 /**
@@ -13,8 +18,15 @@ import view.MainWindow;
 public class Init {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Init.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         MainWindow mainWindow = new MainWindow();
         Controller controller = new Controller(mainWindow);
+        
         mainWindow.setController(controller);
         controller.initialStatus();
         
