79e3f37925f482c22f3851adcf6e02cdb6ea3a10
Fernando GJ <fernandogajib@gmail.com>
2024-09-21 18:43:30 +0200

    record removed

--- src/model/Program.java
+++ src/model/Program.java
@@ -6,9 +6,66 @@
 package model;
 
 import java.net.URL;
+import java.util.Objects;
 
 /**
  *
  * @author Fernando GJ
  */
-public record Program(String name, URL website) {} // end Program
+public class Program {
+    private String name;
+    private URL website;
+
+    public Program(String name, URL website) {
+        this.name = name;
+        this.website = website;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public URL getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(URL website) {
+        this.website = website;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.name);
+        hash = 83 * hash + Objects.hashCode(this.website);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Program other = (Program) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.website, other.website);
+    }
+
+    @Override
+    public String toString() {
+        return "Program{" + "name=" + name + ", website=" + website + '}';
+    }
+   
+} // end Program
